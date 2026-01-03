3e77f132f4fd3931db708c83ba84154eafe321c8
mvazquez-deVOps <mvazquez@juxa.mx>
2026-01-03 14:25:01 -0600

    Se añade configuración Docker

--- src/main/java/com/juxa/legal_advice/LegalAdviceApplication.java
+++ src/main/java/com/juxa/legal_advice/LegalAdviceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-public class 	LegalAdviceApplication {
+public class LegalAdviceApplication {
 
 	public static void main(String[] args) {
 
