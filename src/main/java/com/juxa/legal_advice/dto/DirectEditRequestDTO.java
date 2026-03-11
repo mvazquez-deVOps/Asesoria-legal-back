12922790f85f4fbaee4a99027e1be14b8ed89e42
mvazquez-devOps <mvazquez@juxa.mx>
2026-03-11 13:11:27 -0600

    Se añaden cambios  para conectar el prototipo de integración versión 1

---
+++ src/main/java/com/juxa/legal_advice/dto/DirectEditRequestDTO.java
@@ -0,0 +1,11 @@
+package com.juxa.legal_advice.dto;
+
+import lombok.Data;
+
+
+public class DirectEditRequestDTO {
+    private String fullText;
+    private String selectedText;
+    private String instruction;
+
+}
---
+++ src/main/java/com/juxa/legal_advice/dto/DirectGenerateRequestDTO.java
@@ -0,0 +1,7 @@
+package com.juxa.legal_advice.dto;
+import lombok.Data;
+@Data
+public class DirectGenerateRequestDTO {
+    private String prompt;
+}
+
